4d0ee2cd8af77799ea25ce50c32e1dc4bda5d0d1
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2024-09-14 11:50:06 +0000

    Rearrange import order in semantix/__init__.py

--- semantix/__init__.py
+++ semantix/__init__.py
@@ -1,6 +1,6 @@
 """Semantix is a Python library that give superpowers to your code."""
 
-from semantix.decorators import tool, enhance
+from semantix.decorators import enhance, tool
 from semantix.types.semantic import Semantic, SemanticClass
 
 __all__ = ["Semantic", "enhance", "tool", "SemanticClass"]
